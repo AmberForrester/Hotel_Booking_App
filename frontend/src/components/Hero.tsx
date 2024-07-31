23e7b27801667371e5aebf0f2120ca6f780277ab
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-07-31 00:51:01 -0400

    Header + Hero Component started.

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+
+  return (
+
+    <div className='bg-blue-800 pb-16'>
+        <div className='container mx-atuo flex flex-col gap-2'>
+            <h1 className='text-5xl text-white font-bold'>
+                MERNified Lodging
+            </h1>
+            <p className='text-2xl text-white'>
+                Search low prices on hotels for your dream vacation...
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js, ts, jsx, tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        md: "10rem",
+      },
+    },
   },
   plugins: [],
-}
+};
 
